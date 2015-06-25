bd1682d3c24d7655f39aca466b8f593a7997fa64
Eberhard Beilharz <eb1@sil.org>
2015-06-25 12:32:03 +0200

    Initial commit

---
+++ BuildTasks.TextTemplating/FilterItems.cs
@@ -0,0 +1,44 @@
+﻿// Copyright (c) 2015 SIL International
+// This software is licensed under the MIT License (http://opensource.org/licenses/MIT)
+using System;
+using System.Collections.Generic;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace BuildTasks.TextTemplating
+{
+	public class FilterItems: Task
+	{
+		public ITaskItem[] Include { get; set; }
+
+		public ITaskItem[] Exclude { get; set; }
+
+		public string MetadataName { get; set; }
+		public string MetadataValue { get; set; }
+
+		[Output]
+		public ITaskItem[] Result { get; set; }
+
+		public override bool Execute()
+		{
+			if (Include == null || Include.Length == 0)
+				return true;
+
+			var output = new List<ITaskItem>();
+			foreach (var matchedItem in Include)
+			{
+				var metadata = matchedItem.GetMetadata(MetadataName);
+				if (string.IsNullOrEmpty(metadata))
+					continue;
+				if (!string.IsNullOrEmpty(MetadataValue) && metadata != MetadataValue)
+					continue;
+
+				output.Add(matchedItem);
+			}
+
+			Result = output.ToArray();
+
+			return true;
+		}
+	}
+}
---
+++ BuildTasks.TextTemplating/TransformTemplates.cs
@@ -0,0 +1,58 @@
+﻿// Copyright (c) 2015 SIL International
+// This software is licensed under the MIT License (http://opensource.org/licenses/MIT)
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using Mono.TextTemplating;
+
+namespace BuildTasks.TextTemplating
+{
+	public class TransformTemplates: Task
+	{
+		public ITaskItem[] TemplatesToProcess { get; set; }
+		public ITaskItem[] OutputFiles { get; set; }
+		public bool MinimalRebuildFromTracking { get; set; }
+
+		[Output]
+		public ITaskItem[] GeneratedFiles { get; set; }
+
+		public override bool Execute()
+		{
+			if (TemplatesToProcess == null || OutputFiles == null)
+				return true;
+
+			var retVal = true;
+			var generator = new TemplateGenerator();
+			var output = new List<ITaskItem>();
+
+			for (int i = 0; i < Math.Min(TemplatesToProcess.Length, OutputFiles.Length); i++)
+			{
+				if (MinimalRebuildFromTracking)
+				{
+					var source = new FileInfo(TemplatesToProcess[i].ItemSpec);
+					var dest = new FileInfo(OutputFiles[i].ItemSpec);
+					if (source.LastWriteTimeUtc <= dest.LastWriteTimeUtc)
+					{
+						Log.LogMessage(MessageImportance.Low,
+							"Skipping template '{0}' because its outputs are up-to-date.",
+							TemplatesToProcess[i].ItemSpec);
+						continue;
+					}
+				}
+				Log.LogMessage(MessageImportance.Low, "Processing template '{0}' into '{1}'",
+					TemplatesToProcess[i].ItemSpec, OutputFiles[i].ItemSpec);
+				generator.ProcessTemplate(TemplatesToProcess[i].ItemSpec, OutputFiles[i].ItemSpec);
+				if (generator.Errors.HasErrors)
+				{
+					Log.LogError("Processing '{0}' failed.", TemplatesToProcess[i].ItemSpec);
+					retVal = false;
+				}
+				output.Add(OutputFiles[i]);
+			}
+			GeneratedFiles = output.ToArray();
+			return retVal;
+		}
+	}
+}
